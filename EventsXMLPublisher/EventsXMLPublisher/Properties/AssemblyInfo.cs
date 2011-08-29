5cc3fd88f8ef88aae2bcf20d1d78f4cc96eb346f
millsy <millsy@openspan.com>
2011-08-29 17:44:17 +0100

    Initial Release

---
+++ EventsXMLPublisher/EventsXMLPublisher/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EventsXMLPublisher")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("OpenSpan, Inc.")]
+[assembly: AssemblyProduct("EventsXMLPublisher")]
+[assembly: AssemblyCopyright("Copyright © OpenSpan, Inc. 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("88078171-3bed-44e2-8a29-a810113ebe44")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EventsXMLPublisher/EventsXMLPublisher/XMLFileWriter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Design;
+
+using OpenSpan.ComponentModel;
+using OpenSpan.Design;
+using OpenSpan.Diagnostics;
+using OpenSpan.TypeManagement;
+using System.ComponentModel;
+using System.IO;
+using OpenSpan.Events;
+
+namespace EventsXMLPublisher
+{
+    [ToolboxBitmap(typeof(XMLFileWriter), "EventConnector.ico")]
+    [ObjectExplorerGroupMember(ObjectExplorerGroupName.Events)]
+    public partial class XMLFileWriter : BaseConnector
+    {
+        public XMLFileWriter()
+        {
+
+        }
+
+        private string mFilePath;
+        private bool mLoggingEnabled;
+
+        [RefreshProperties(RefreshProperties.All)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [MemberVisibility(MemberVisibilityLevel.DefaultOn)]
+        public string FilePath
+        {
+            get { return mFilePath; }
+            set
+            {
+                if (value == null || !Directory.Exists(value))
+                    value = string.Empty;
+
+                mFilePath = value;
+            }
+        }
+
+        [Browsable(true)]
+        [MemberVisibility(MemberVisibilityLevel.DefaultOn)]
+        [Description("Enables/disables the logging of events to the designated dir")]
+        public bool LoggingEnabled
+        {
+            get { return mLoggingEnabled; }
+            set { mLoggingEnabled = value; }
+        }
+
+        protected override void ProcessEvent(EventOccurredArgs args)
+        {
+            // if logging is disabled, don't write this out...
+            if (mLoggingEnabled == false) return;
+
+            if (mFilePath != string.Empty)
+            {
+                LogEvent(args, mFilePath);
+            }
+        }
+
+        private void LogEvent(EventOccurredArgs args, string fileName)
+        {
+            try
+            {
+                string eventStr = EventOccurredArgs.Serialize(args);
+                Guid guid = Guid.NewGuid();
+
+                File.WriteAllText(Path.Combine(fileName, guid.ToString() + ".xml"), eventStr);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
